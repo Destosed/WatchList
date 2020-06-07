b9a3a382f9481f70229ebad16baf4388ec58dd9f
Nikita <luzhbinnikita@gmail.com>
2020-06-07 06:47:02 +0300

    Added Wish List Screen

--- WatchList/Modules/WishListScreen/WishListView.swift
+++ WatchList/Modules/WishListScreen/WishListView.swift
@@ -1,8 +1,140 @@
 import UIKit
+import Kingfisher
+import PromiseKit
 
 class WishListView: UIViewController {
 
+    // MARK: - Nested Types
+    
+    enum Constants {
+        
+        // MARK: - Type Properties
+        
+        static let mediaItemCell = "MediaItemCell"
+    }
+    
+    // MARK: - Instance Properties
+
+    @IBOutlet private weak var tableView: UITableView!
+    
+    var movies: [MovieInfo] = []
+    
+    // MARK: - Insatnce Methods
+    
+    @IBAction private func onMoreButtonTouchUpInside(_ sender: Any) {
+        self.presentMoreOptionsAlert()
+    }
+    
+    // MARK: -
+    
+    private func fetchMovies() {
+        firstly {
+            NetworkManager.shared.getMovies(category: .all, filterType: .wishList)
+        }.done { movies in
+            self.movies = movies
+        }.ensure {
+            self.tableView.reloadData()
+        }.catch { error in
+            AlertService.shared.showError(on: self,
+                                          title: "Error",
+                                          message: error.localizedDescription,
+                                          complition: nil)
+        }
+    }
+    
+    private func presentMoreOptionsAlert() {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let addAction = UIAlertAction(title: "Add", style: .default, handler: { [weak self] _ in
+            let searchStoryboard = UIStoryboard(name: "Search", bundle: nil)
+            
+            guard let searchController = searchStoryboard.instantiateInitialViewController() as? SearchView else {
+                return
+            }
+            
+            searchController.onMovieCellSelected = { movie in
+                
+            }
+            
+            self?.present(searchController, animated: true, completion: nil)
+        })
+        
+        alertController.addAction(addAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK: -
+    
+    private func configureTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        let nibMediaItemCell = UINib(nibName: Constants.mediaItemCell, bundle: nil)
+        self.tableView.register(nibMediaItemCell, forCellReuseIdentifier: Constants.mediaItemCell)
+        
+        self.tableView.separatorStyle = .none
+    }
+    
+    private func configureMediaItemCell(cell: MediaItemCell, with movie: MovieInfo) {
+        cell.selectionStyle = .none
+        
+        cell.title = movie.ruName
+        cell.subTitle = movie.engName
+        cell.note = movie.description
+        
+        if let intRating = Int(movie.rating ?? "") {
+            cell.rating = Double(intRating)
+        }
+        
+        if let imageURL = URL(string: movie.posterURL ?? "") {
+            let resource = ImageResource(downloadURL: imageURL,
+                                         cacheKey: movie.posterURL)
+            cell.imageViewTarget.kf.setImage(with: resource)
+        }
+    }
+    
+    // MARK: - UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.configureTableView()
+        self.fetchMovies()
     }
 }
+
+// MARK: - UITableViewDataSource
+
+extension WishListView: UITableViewDataSource {
+    
+    // MARK: - Instance Methods
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.mediaItemCell) as! MediaItemCell
+        self.configureMediaItemCell(cell: cell, with: self.movies[indexPath.row])
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension WishListView: UITableViewDelegate {
+    
+    // MARK: - Instance Methods
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 170.0
+    }
+}
+
